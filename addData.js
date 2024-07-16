4c5f225404939a1206bd2107063281f9284a597a
valentin-orrit <vorrit@gmail.com>
2024-07-16 15:19:29 +0200

    coded dbFileCheck.js, queryDB.js and started appData.js

---
+++ addData.js
@@ -0,0 +1,13 @@
+import inquirer from 'inquirer'
+import fs from 'fs'
+import { v4 as uuidv4 } from 'uuid'
+import queryDB from './queryDB'
+
+
+export default async function addData(info) {
+    try {
+
+    } catch (error) {
+        console.log('Something went wrong!', error)
+    }
+}
---
+++ dbFileCheck.js
@@ -0,0 +1,10 @@
+import fs from 'fs'
+import { exit } from 'process'
+
+export default function dbFileChek() {
+    if (!fs.existsSync('db.json')) {
+        console.log('Database is Empty!')
+        exit(1)
+    }
+}
+
